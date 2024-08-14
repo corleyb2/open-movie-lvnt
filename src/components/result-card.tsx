ba5ba7ea5c60dface0bb1427953a77c2f6d97993
Brian Corley <56326492+corleyb2@users.noreply.github.com>
2024-08-13 22:40:07 -0400

    chore: card style w chips

--- src/components/result-card.tsx
+++ src/components/result-card.tsx
@@ -1,25 +1,60 @@
 import { OpenMovieResult } from "@/types";
-import { Badge, Button, Card, CardSection, Group, Image, Text } from "@mantine/core";
+import {
+  Badge,
+  Button,
+  Card,
+  CardSection,
+  Flex,
+  Group,
+  Image,
+  Text,
+} from "@mantine/core";
+import { useMemo } from "react";
 
 interface ResultCardProps {
   cardData: OpenMovieResult;
 }
 
 export function ResultCard({ cardData }: ResultCardProps) {
-  const {Poster, Title, Type, Year} = cardData
+  const { Poster, Title, Type, Year } = cardData;
+
+  const colorForType = useMemo(() => {
+    if (Type === "episode" ){
+        return "maroon"
+    } else if (Type === "movie"){
+      return "green"
+    } else {
+      return "blue"
+    }
+  }, [Type])
+
   return (
-    <Card shadow="sm" padding="sm" radius="md" withBorder style={{flexDirection: "row", justifyContent: "space-between"}}>
-        {/* <Image
-          src={cardData.Poster}
-          height={120}
-          alt={`${cardData.Title} poster`}
-        /> */}
+    <Card
+      shadow="sm"
+      padding="sm"
+      radius="md"
+      withBorder
+      style={{
+        flexDirection: "row",
+        alignContent: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <Flex style={{position: "relative"}}>
+      <Image
+        src={cardData.Poster}
+        height={120}
+        alt={`${cardData.Title} poster`}
+      />
+       <Badge color={colorForType} style={{position: "absolute", top: -10, right: -10}}>{Type}</Badge>
+       </Flex>
       <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>{Title} - ({Year})</Text>
-        <Badge color="pink">{Type}</Badge>
+        <Text fw={500}>
+          {Title} - ({Year})
+        </Text>
+       
       </Group>
       <Button>Go to Movie</Button>
-
     </Card>
   );
 }
